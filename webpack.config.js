f12044c4314bede957f577b1acc1a6db03c3df6d
andrewkillinger <andrew.killinger@gmail.com>
2025-09-01 11:17:46 -0700

    Use remote icon image

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
     new WasmPackPlugin({ crateDirectory: path.resolve(__dirname, "crate") }),
     new CopyWebpackPlugin([
       "index.html",
-      "js/styles.css",
+      "styles.css",
       "manifest.json",
       "assets/*",
     ]),
